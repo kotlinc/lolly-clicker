c2154401f1dc4e229cec61abe5b1d58895fc2018
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 18:18:05 +1000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -1,5 +1,12 @@
 import { lollies } from './game.js'
 import { increment } from './upgrade.js'
 
+let passiveLollies = 0;
+
 const gSnakeButton = document.getElementById("gSnake")
 const gSnakeCost = document.getElementById("gSnake-price")
+let gSnakeCount = document.getElementById("gSnake-count")
+
+gSnakeButton.addEventListener("click", () => {
+  
+})
