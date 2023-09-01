08e09f48314f455008e8167a6374d81ed41a8103
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 18:15:27 +1000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -1,1 +1,5 @@
 import { lollies } from './game.js'
+import { increment } from './upgrade.js'
+
+const gSnakeButton = document.getElementById("gSnake")
+const gSnakeCost = document.getElementById("gSnake-price")
