00bda45f25003d34689836ced824998b677f3674
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 19:12:12 +1000

    Update game.js

--- game.js
+++ game.js
@@ -15,6 +15,13 @@ let gSnakesOwned = 0;
 let gSnakeRate = 0.2;
 let gSnakePrice = 10;
 
+const hmLollyButton = document.getElementById("hmLolly")
+const hmLollyCost = document.getElementById("hmLolly-price")
+let hmLolllyCount = document.getElementById("hmLolly-count")
+let hmLolliesOwned = 0;
+let hmLollyRate = 3;
+let hmLollyPrice = 100;
+
 lollyButton.addEventListener("click", () => {
   lollies += clickPower;
   lollyCount.textContent = lollies;
@@ -24,12 +31,29 @@ gSnakeButton.addEventListener("click", () => {
   console.log("Event listener worked")
   if (lollies >= gSnakePrice) {
     console.log("Sufficient lollies")
+    gSnakesOwned++
     lollies -= gSnakePrice;
     gSnakePrice *= increment;
     passiveLollies += gSnakeRate;
     gSnakeCost.textContent = gSnakePrice.toFixed(1);
     lollyCount.textContent = lollies.toFixed(1);
     passiveLollyCount.textContent = passiveLollies.toFixed(1);
+    gSnakeCount.textContent = gSnakesOwned;
+  }
+})
+
+hmLollyButton.addEventListener("click", () => {
+  console.log("Event listener worked")
+  if (lollies >= mhLollyPrice) {
+    console.log("Sufficient lollies")
+    hmLolliesOwned++
+    lollies -= hmLollyPrice;
+    hmLollyPrice *= increment;
+    passiveLollies += hmLollyRate;
+    gSnakeCost.textContent = mhLollyPrice.toFixed(1);
+    lollyCount.textContent = lollies.toFixed(1);
+    passiveLollyCount.textContent = passiveLollies.toFixed(1);
+    hmLolllyCount.textContent = hmLolliesOwned;
   }
 })
 
