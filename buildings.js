fa5b67f02606089815a54ea296e6a2933dbba209
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 18:23:23 +1000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -6,7 +6,17 @@ let passiveLollies = 0;
 const gSnakeButton = document.getElementById("gSnake")
 const gSnakeCost = document.getElementById("gSnake-price")
 let gSnakeCount = document.getElementById("gSnake-count")
+let gSnakesOwned = 0;
+let gSnakeRate = 0.2;
+let gSnakePrice = 10;
 
 gSnakeButton.addEventListener("click", () => {
-  
+  if (lollies >= gSnakePrice) {
+    lollies -= gSnakePrice;
+    
+  }
 })
+
+setInterval(() => {
+  lollies += passiveLollies;
+}, 1000);
