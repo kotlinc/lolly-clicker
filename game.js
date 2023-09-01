42697ac6d674fff55503079329c133fced64a4b6
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 18:53:32 +1000

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,37 @@
-import { clickPower } from './upgrades.js'
-
 let lollies = 0;
 const lollyButton = document.getElementById("lButton")
-export const lollyCount = document.getElementById("lollies")
+const lollyCount = document.getElementById("lollies")
+
+const passiveLollyCount = document.getElementById("lolliesPSec")
+let passiveLollies = 0;
+
+const gSnakeButton = document.getElementById("gSnake")
+const gSnakeCost = document.getElementById("gSnake-price")
+let gSnakeCount = document.getElementById("gSnake-count")
+let gSnakesOwned = 0;
+let gSnakeRate = 0.2;
+let gSnakePrice = 10;
 
 lollyButton.addEventListener("click", () => {
   lollies += clickPower;
   lollyCount.textContent = lollies;
 });
 
-export { lollies }
+gSnakeButton.addEventListener("click", () => {
+  console.log("Event listener worked")
+  if (lollies >= gSnakePrice) {
+    console.log("Sufficient lollies")
+    lollies -= gSnakePrice;
+    gSnakePrice *= increment;
+    passive += gSnakeRate;
+    gSnakeCost.textContent = gSnakePrice.toFixed(1);
+    lollyCount.textContent = lollies.toFixed(1);
+    passiveLollyCount.textContent = passiveLollies.toFixed(1);
+  }
+})
+
+setInterval(() => {
+  console.log("Lollies Per Second is working")
+  lollies += passiveLollies;
+  lollyCount.textContent = lollies.toFixed(1);
+}, 1000);
