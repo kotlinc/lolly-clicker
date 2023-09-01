a077d34286a2f26a543dc57494010c4f7b91904a
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-09-01 18:00:17 +1000

    Create game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+import { clickPower } from './upgrades.js'
+
+let lollies = 0;
+const lollyButton = document.getElementById("lButton")
+const lollyCount = document.getElementById("lollies")
+
+lollyButton.addEventListener("click" () => {
+  lollies += clickPower;
+  lollyCount.textContent = lollies;
+})
